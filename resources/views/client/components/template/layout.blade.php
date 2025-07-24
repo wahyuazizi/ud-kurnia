63fd8acf89a8ca4d98065545fd89bb0d0f1f3e5e
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-07-24 22:44:50 +0800

    update payment

--- resources/views/client/components/template/layout.blade.php
+++ resources/views/client/components/template/layout.blade.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
